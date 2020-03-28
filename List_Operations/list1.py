9f8e8a2d0f8d112458768096a73fd2ded5ee8206
EswarAleti <eswaraleti143@gmail.com>
2020-03-28 10:05:15 +0530

    Basic list operations

---
+++ List_Operations/list1.py
@@ -0,0 +1,35 @@
+def display_list(label,items):
+	print(label)
+	for element in items:
+		print(element)
+
+foods=['pizza', 'salad', 'hamburger', 'steak', 'apple', 'orange']
+
+display_list("foods in original order:", foods)
+
+foods.sort()
+display_list("foods in ascending alphabetical order:",foods)
+
+foods.sort(reverse=True)
+display_list("foods in descending alphabetical order:",foods)
+
+foods2=sorted(foods)
+display_list("foods2 in ascending alphabetical order:",foods2)
+display_list("foods still in descending alphabetical order:",foods)
+
+foods.reverse()
+display_list("foods in ascending alphabetical order:",foods)
+
+foods.append('carrot')
+foods.append('milk')
+display_list("sorted foods with carrots and milk appended to end:",foods)
+
+foods.sort()
+display_list("foods in ascending alphabetical order:",foods)
+
+pizza_index = foods.index('pizza')
+print("Pizza is at ",pizza_index)
+
+foods.insert(pizza_index,"fries")
+pizza_index = foods.index('pizza')
+print("Pizza is at ",pizza_index)
