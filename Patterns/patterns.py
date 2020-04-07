af9de277c2bf75ac13961c0b983d62f06a805493
EswarAleti <eswaraleti143@gmail.com>
2020-04-07 12:46:06 +0530

    Iterative approach

--- Patterns/patterns.py
+++ Patterns/patterns.py
@@ -1,18 +1,16 @@
 def patterns(string):
-	#Initialize count to 0
-	count=0
-	#Traverse till index len(string)-2
-	for i in range(0,len(string)-2):
-		#If 1st and 3rd letters matched and 1st and 2nd letters doesn't matched
-		if string[i]==string[i+2] and string[i]!=string[i+1]:
-			#Increment the count
-			count+=1
-	#return the count
-	return count
+   #Initialize count to 0
+   count=0
+   #Traverse till index len(string)-2
+   for i in range(0,len(string)-2):
+       #If 1st and 3rd letters matched and 1st and 2nd letters doesn't matched
+       if string[i]==string[i+2] and string[i]!=string[i+1]:
+           #Increment the count
+           count+=1
+   #return the count
+   return count
 #Function calling
 first = 'axab'
 print(patterns(first))
 second = 'cdcece'
 print(patterns(second))
-
-
