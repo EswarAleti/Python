116dccaf98d9b7a4c9f3c213d32f1dd8b746c3bf
EswarAleti <eswaraleti143@gmail.com>
2020-05-05 11:15:20 +0530

    Scores has been sorted by including sort() function

--- number_manipulations/scores.py
+++ number_manipulations/scores.py
@@ -24,21 +24,31 @@ def is_nice(n):
 	return n==0
 
 def update_scores(scores):
+	# This score stores updated scores
 	updated_scores = []
+	# For each score in scores
 	for score in scores:
 		temp = score
+		# If score is palindrome then double the score
 		if is_palindrome(score):
 			temp*=2
+		# If score is near prime then double the score
 		if is_near_prime(score):
 			temp*=2
+		# If score is nice then triple the score
 		if is_nice(score):
 			temp*=3
+		# Append the score and updated score to the list
 		updated_scores.append(score)
 		updated_scores.append(temp)
+	# Return the list
 	return updated_scores
-
+#User input
 scores = input('Enter scores: ').split()
+# Convert all scores to integers
 scores = [int(x) for x in scores]
+scores.sort()
+# Function calling
 updated_scores = update_scores(scores)
 for i in range(0,len(updated_scores),2):
 	print('{0},{1}'.format(updated_scores[i],updated_scores[i+1]),end=' ')
